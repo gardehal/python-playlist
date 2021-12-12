d6c88c87871e8c7b5e6be9f70add13792c3e52eb
Alexander <alex.thogar@gmail.com>
2021-12-12 18:08:03 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,20 +1,28 @@
-import re
 import os
 import sys
+import json
+from model.VideoSource import VideoSource
 from myutil.Util import *
 from myutil.DateTimeObject import *
 from pytube import YouTube
 from dotenv import load_dotenv
+
 load_dotenv()
 debug = False
+settingsFilename = "settings.json"
+sourcesFilename = "queuesources.json"
+queueFilename = "queue.txt"
 
 os.system("") # Needed to "trigger" coloured text
 helpFlags = ["-help", "-h"]
 testFlags = ["-test", "-t"]
-downloadFlags = ["-download", "-d"]
-editSpliceSwitches = ["splice", "spl", "join", "j"]
-
-youTubeFlags = ["-youtube", "-yt"]
+playFlags = ["-play", "-p"]
+listQueueFlags = ["-list", "-l"]
+addVideoFlags = ["-add", "-a"]
+removeVideoFlags = ["-remove", "-rm", "-r"]
+addSourcesFlags = ["-addsource", "-as"]
+removeSourceFlags = ["-removesource", "-rms", "-rs"]
+fetchSourceFlags = ["-fetch", "-f", "-update", "-u"]
 
 class Main:
     def main():
@@ -24,6 +32,8 @@ class Main:
 
         if(argC < 2):
             Main.printHelp()
+            
+        Main.createLocalFilesIfNone()
 
         while argIndex < argC:
             arg = sys.argv[argIndex].lower()
@@ -37,8 +47,13 @@ class Main:
 
                 quit()
 
-            elif(arg in downloadFlags):
+            elif(arg in addSourcesFlags):
                 args = extractArgs(argIndex, argV)
+                
+                sourcesAdded = Main.addSource(args)
+                if(sourcesAdded != None and sourcesAdded > 0):
+                    printS("Added ", sourcesAdded, " new sources", color=colors["OKGREEN"])
+                    continue
 
                 argIndex += len(args) + 1
                 continue
@@ -48,6 +63,73 @@ class Main:
                 printS("Argument not recognized: \"", arg, "\", please see documentation or run with \"-help\" for help.", color=colors["WARNING"])
 
             argIndex += 1
+            
+    def createLocalFilesIfNone() -> bool:
+        """"
+        Create local files used for storing settings, video ques, sources etc.
+        \nreturns success = true
+        """
+        
+        files = [settingsFilename, sourcesFilename, queueFilename]
+        
+        for file in files:
+            open(file, "a")
+            
+        return True
+    
+    def toDict(obj: object) -> dict:
+        """ 
+        Converts objects to dictionaries.
+        \nSource: https://www.codegrepper.com/code-examples/whatever/python+nested+object+to+dict
+        """
+        
+        if not  hasattr(obj,"__dict__"):
+            return obj
+        
+        result = {}
+        for key, val in obj.__dict__.items():
+            if key.startswith("_"):
+                continue
+            
+            element = []
+            if isinstance(val, list):
+                for item in val:
+                    element.append(Main.toDict(item))
+            else:
+                element = Main.toDict(val)
+                
+            result[key] = element
+            
+        return result
+    
+    def toJson(obj):
+        dict = Main.toDict(obj)
+        return json.dumps(dict, default=str)
+            
+    def addSource(sources) -> int:
+        """
+        Add video source(s) to list of watched sources.
+        \nreturns number of added sources
+        """
+        
+        newSources = []
+        
+        for source in sources:
+            isUrl = validators.url(source)
+            url = source if isUrl else None
+            dir = source if os.path.exists(source) else None
+            if(url == None and dir == None):
+                printS("The source: ", source, "is not a valid URL or directory path.", color=colors["ERROR"])
+                continue
+            
+            dto = DateTimeObject()
+            newSource = VideoSource("name", url, dir, isUrl, True, dto, dto)
+            newSources.append(newSource)
+            
+        updatedSourcesJson = Main.toJson(newSource)
+        f = open(sourcesFilename, "a")
+        f.writelines(updatedSourcesJson)
+        f.close        
 
     def printHelp():
         """
@@ -62,9 +144,16 @@ class Main:
 
         printS(helpFlags, ": Prints this information about input arguments.")
         printS(testFlags, ": A method of calling experimental code (when you want to test if something works).")
+        # printS(testFlags, " + [args]: Details.")
+        # printS("\t", testSwitches, " + [args]: Details.")
 
-        printS(downloadFlags, " + [URL]: Downloads a file from the URL argument.")
-        printS("\t", editSpliceSwitches, " + [filepaths to splice]: Splice/join two or more videos or audio files after each other, in given order.")
+        printS(playFlags, ": Starts playing the queue.")
+        printS(listQueueFlags, ": List videos in queue.")
+        printS(addVideoFlags, " + [1+ URLs or paths to files]: Add videos to queue.")
+        printS(removeVideoFlags, " + [index]: Remove videos from queue.")
+        printS(addSourcesFlags, " + [1+ URLs or paths to directories]: Adds a video source to list of watched sources.")
+        printS(removeSourceFlags, " + [1+ URLs or paths to directories]: Removes video source(s) from list of watched sources.")
+        printS(fetchSourceFlags, ": Update queue with videos from list of watched sources.")
         
 if __name__ == "__main__":
     Main.main()

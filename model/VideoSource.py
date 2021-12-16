90e5e985821b44472951ea9f30a6c3a597d66ab3
Alexander <alex.thogar@gmail.com>
2021-12-17 00:32:13 +0100

    Update VideoSource.py

--- model/VideoSource.py
+++ model/VideoSource.py
@@ -1,11 +1,13 @@
 from myutil import DateTimeObject
+from enums.VideoSourceType import VideoSourceType
 
 class VideoSource:
-    def __init__(self, name: str, url: str, directory: str, isWebSource: bool, enableFetch: bool, lastFetched: DateTimeObject, datetimeAdded: DateTimeObject):
+    def __init__(self, name: str, url: str, directory: str, isWebSource: bool, videoSourceType: VideoSourceType, enableFetch: bool, lastFetched: DateTimeObject, datetimeAdded: DateTimeObject):
         self.name: str = name
         self.url: str = url
         self.directory: str = directory
         self.isWebSource: bool = isWebSource
+        self.videoSourceType: VideoSourceType = videoSourceType
         self.enableFetch: bool = enableFetch
         self.lastFetched: DateTimeObject = lastFetched
         self.datetimeAdded: DateTimeObject = datetimeAdded

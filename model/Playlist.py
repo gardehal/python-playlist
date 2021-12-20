0e9ef6e914cd9d2995748d6b784b273d8db4f02e
Alexander <alex.thogar@gmail.com>
2021-12-20 03:11:26 +0100

    Update Playlist.py

--- model/Playlist.py
+++ model/Playlist.py
@@ -1,12 +1,12 @@
 from typing import List
-from VideoSource import VideoSource
-from QueueVideo import QueueVideo
-from myutil import DateTimeObject
+from model.VideoSource import VideoSource
+from model.QueueVideo import QueueVideo
+from datetime import datetime
 
-class Playlist:
-    def __init__(self, name: str videos: List(QueueVideo), videoSource: VideoSource, lastUpdated: DateTimeObject, lastWatchedIndex: int):
+class Playlist():
+    def __init__(self, name: str videos: List[QueueVideo], videoSource: VideoSource, lastUpdated: datetime, lastWatchedIndex: int):
         self.name: str = name
         self.videoSource: VideoSource = videoSource
-        self.videos: List(QueueVideo) = videos
-        self.lastUpdated: DateTimeObject = lastUpdated
+        self.videos: List[QueueVideo] = videos
+        self.lastUpdated: datetime = lastUpdated
         self.lastWatchedIndex: int = lastWatchedIndex

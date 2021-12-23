ef6a9f6fc2660c9b702484c63d4f24d5492ade77
grdAll <alex.thogar@gmail.com>
2021-12-23 05:23:13 +0100

    Update QueueVideo.py

--- model/QueueVideo.py
+++ model/QueueVideo.py
@@ -4,11 +4,11 @@ class QueueVideo:
     def __init__(self, 
                  videoName: str = None, 
                  sourceUri: str = None, 
-                 watched: bool = None, 
+                 watched: datetime = None, 
                  datetimeAdded: datetime = None, 
                  videoSourceName: str = None):
         self.videoName: str = videoName
         self.sourceUri: str = sourceUri
-        self.watched: bool = watched
+        self.watched: datetime = watched
         self.datetimeAdded: datetime = datetimeAdded
         self.videoSourceName: str = videoSourceName

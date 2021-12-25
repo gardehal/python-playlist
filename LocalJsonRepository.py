3ac08fa8083f72695288b1ea5e7701782536b711
grdAll <alex.thogar@gmail.com>
2021-12-26 00:08:51 +0100

    Update LocalJsonRepository.py

--- LocalJsonRepository.py
+++ LocalJsonRepository.py
@@ -1,30 +1,46 @@
 from model.Playlist import *
 from myutil.Util import *
+from JsonUtil import *
 from typing import List, TypeVar
 
 T = TypeVar("T")
 
-class PlaylistService:
+class LocalJsonRepository():
     debug: bool = False
-    storagePath: str = None
+    storagePath: str = "./"
 
-    def add(entity: T) -> bool:
+    def __init__(self, 
+                 debug: bool = False,
+                 storagePath: str = "./"):
+        LocalJsonRepository.debug: bool = debug
+        LocalJsonRepository.storagePath: str = storagePath
+
+    def add(self, entity: T, filename: str = None) -> bool:
         """
         Add a new entity using local JSON files for storage.
 
         Args:
             entity (T): entity to add
+            filename (str): filename to store file in, defaults to id + "".json"
 
         Returns:
             bool: success = True
         """
 
         try:
+            entityAsJson = JsonUtil.toDict(entity)
+            _filename = filename
+            if(_filename == None):
+                _filename = entity.id + ".json"
+
+            path = os.path.join(LocalJsonRepository.storagePath, _filename)
+            with open(path, "a") as file:
+                json.dump(entityAsJson, file, indent=4, default=str)
 
             return True
         except Exception:
-            if(debug): printS("Debug: Error adding", color=colors["FAIL"])
-            printS(sys.exc_info(), color=colors["WARNING"])
+            if(LocalJsonRepository.debug): printS(sys.exc_info(), color=colors["WARNING"])
+            printS("Error adding", color=colors["FAIL"])
             return False
 
     def get(id: uuid) -> T:
@@ -42,8 +58,8 @@ class PlaylistService:
             
             return None
         except Exception:
-            if(debug): printS("Debug: Error getting", color=colors["FAIL"])
-            printS(sys.exc_info(), color=colors["WARNING"])
+            if(debug): printS(sys.exc_info(), color=colors["WARNING"])
+            printS("Error getting", color=colors["FAIL"])
             return None
 
     def getAll() -> List[T]:
@@ -58,8 +74,8 @@ class PlaylistService:
             
             return List[T]
         except Exception:
-            if(debug): printS("Debug: Error getting all", color=colors["FAIL"])
-            printS(sys.exc_info(), color=colors["WARNING"])
+            if(debug): printS(sys.exc_info(), color=colors["WARNING"])
+            printS("Error getting all", color=colors["FAIL"])
             return List[T]
 
     def update(id: uuid) -> bool:
@@ -77,8 +93,8 @@ class PlaylistService:
             
             return True
         except Exception:
-            if(debug): printS("Debug: Error updating", color=colors["FAIL"])
-            printS(sys.exc_info(), color=colors["WARNING"])
+            if(debug): printS(sys.exc_info(), color=colors["WARNING"])
+            printS("Error updating", color=colors["FAIL"])
             return False
 
     def remove(id: uuid) -> bool:
@@ -96,6 +112,6 @@ class PlaylistService:
             
             return True
         except Exception:
-            if(debug): printS("Debug: Error removeing", color=colors["FAIL"])
-            printS(sys.exc_info(), color=colors["WARNING"])
+            if(debug): printS(sys.exc_info(), color=colors["WARNING"])
+            printS("Error removeing", color=colors["FAIL"])
             return False

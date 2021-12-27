0e65139357f9b7a2ca4fb3bb16b249653e1f87f4
grdAll <alex.thogar@gmail.com>
2021-12-27 03:47:18 +0100

    Update Playlist.py

--- model/Playlist.py
+++ model/Playlist.py
@@ -5,14 +5,14 @@ import uuid
 class Playlist():
     def __init__(self, 
                  name: str = None, 
-                 queueVideoIds: List[str] = List[str], 
-                 videoSourceCollectionId: str = None, 
+                 streamIds: List[str] = List[str], 
+                 sourceCollectionId: str = None, 
                  lastUpdated: datetime = datetime.now(), 
                  lastWatchedIndex: int = None,
                  id: str = str(uuid.uuid4())):
         self.name: str = name
-        self.queueVideoIds: List[str] = queueVideoIds
-        self.videoSourceCollectionId: str = videoSourceCollectionId
+        self.streamIds: List[str] = streamIds
+        self.sourceCollectionId: str = sourceCollectionId
         self.lastUpdated: datetime = lastUpdated
         self.lastWatchedIndex: int = lastWatchedIndex
         self.id: str = id

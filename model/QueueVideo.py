7e440301ad91cf2d012c31847ef97fa4ec986710
grdAll <alex.thogar@gmail.com>
2021-12-28 16:29:14 +0100

    Update QueueVideo.py

--- model/QueueVideo.py
+++ model/QueueVideo.py
@@ -3,17 +3,17 @@ import uuid
 
 class QueueVideo:
     def __init__(self, 
-                 videoName: str = None, 
+                 name: str = None, 
                  uri: str = None, 
                  isWeb: bool = False, 
                  watched: datetime = None, 
                  datetimeAdded: datetime = datetime.now(), 
-                 videoSourceId: str = None,
+                 sourceId: str = None,
                  id: str = str(uuid.uuid4())):
-        self.videoName: str = videoName
+        self.name: str = name
         self.uri: str = uri
         self.isWeb: bool = isWeb
         self.watched: datetime = watched
         self.datetimeAdded: datetime = datetimeAdded
-        self.videoSourceId: str = videoSourceId
+        self.sourceId: str = sourceId
         self.id: str = id

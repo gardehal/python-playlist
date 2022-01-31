1594857feba1d09651db241afe5f194c890327f5
Alexander <alex.thogar@gmail.com>
2022-01-31 06:38:51 +0100

    Added sectioned help-command getters

--- Utility.py
+++ Utility.py
@@ -18,6 +18,43 @@ PLAYED_ALWAYS_WATCHED = eval(os.environ.get("PLAYED_ALWAYS_WATCHED"))
 WATCHED_LOG_FILEPATH = os.environ.get("WATCHED_LOG_FILEPATH")
 BROWSER_BIN = os.environ.get("BROWSER_BIN")
 
+# General
+helpFlags = ["-help", "-h"]
+testFlags = ["-test", "-t"]
+editFlags = ["-edit", "-e"]
+searchFlags = ["-search", "-s"]
+
+# Playlist
+addPlaylistFlags = ["-addplaylist", "-apl", "-ap"]
+addPlaylistFromYouTubeFlags = ["-addplaylistfromyoutube", "-apfy", "-fromyoutube", "-fyt", "-fy"]
+deletePlaylistFlags = ["-deleteplaylist", "-dpl"]
+restorePlaylistFlags = ["-restoreplaylist", "-rpl", "-rp"]
+listPlaylistFlags = ["-listplaylist", "-lpl", "-lp"]
+detailsPlaylistFlags = ["-detailsplaylist", "-dpl", "-dp"]
+fetchPlaylistSourcesFlags = ["-fetch", "-f", "-update", "-u"]
+prunePlaylistFlags = ["-prune"]
+purgePlaylistFlags = ["-purge"]
+resetPlaylistFetchFlags = ["-reset"]
+playFlags = ["-play", "-p"]
+quitSwitches = ["quit", "q", "exit", "end"]
+skipSwitches = ["skip", "s"]
+addCurrentToPlaylistSwitches = ["addto", "at"]
+printPlaybackDetailsSwitches = ["detailsprint", "details", "print", "dp"]
+helpSwitches = ["help", "h"]
+
+# Stream
+addStreamFlags = ["-add", "-a"]
+deleteStreamFlags = ["-delete", "-dm", "-d"]
+restoreStreamFlags = ["-restore", "-r"]
+# Sources
+addSourcesFlags = ["-addsource", "-as"]
+deleteSourceFlags = ["-deletesource", "-ds"]
+restoreSourceFlags = ["-restoresource", "-rs"]
+listSourcesFlags = ["-listsources", "-ls"]
+# Meta
+listSettingsFlags = ["-settings", "-secrets"]
+listSoftDeletedFlags = ["-listsoftdeleted", "-listdeleted", "-lsd", "-ld"]
+
 class Utility():
 
     def getPageTitle(self, url: str) -> str:
@@ -124,12 +161,12 @@ class Utility():
                 
         return True
     
-    def getAllSettingsAsString(self):
+    def getAllSettingsAsString(self) -> str:
         """
         Print settings in .env settings/secrets file.
 
         Returns:
-            None: None
+            str: a string of all settings/secrets in project from .env.
         """
 
         printS("DEBUG: ", DEBUG,
@@ -140,3 +177,129 @@ class Utility():
                "\n", "PLAYED_ALWAYS_WATCHED: ", PLAYED_ALWAYS_WATCHED,
                "\n", "WATCHED_LOG_FILEPATH: ", WATCHED_LOG_FILEPATH,
                "\n", "BROWSER_BIN: ", BROWSER_BIN)
+        
+    def getHelpString(self) -> str:
+        """
+        Get all help-info.
+
+        Returns:
+            str: a string of all help-info
+        """
+
+        _result = ""
+        _result += self.getGeneralHelpString()
+        _result += self.getPlaylistHelpString()
+        _result += self.getQueueStreamHelpString()
+        _result += self.getStreamSourceHelpString()
+        _result += self.getMetaHelpString()
+        
+        return _result
+        
+    def getGeneralHelpString(self) -> str:
+        """
+        Get the general overhead help-print as a string.
+
+        Returns:
+            str: a string of general overhead info
+        """
+
+        _result = ""
+        _result += "\n--- Help ---"
+        _result += "\nArguments marked with ? are optional."
+        _result += "\nAll arguments that triggers a function start with dash(-)."
+        _result += "\nAll arguments must be separated by space only."
+        _result += "\nWhen using an index or indices, format with with an \"i\" followed by the index, like \"i0\"."
+        _result += "\n\n"
+
+        _result += "\n" + helpFlags + ": Prints this information about input arguments."
+        _result += "\n" + testFlags + ": A method of calling experimental code (when you want to test if something works)."
+        _result += "\n" + editFlags + " [playlistId or index: str]: Opens the file with Playlist."
+        _result += "\n" + searchFlags + " [searchTerm: str] [? includeSoftDeleted: bool]: Search all Playlists, QueueStreams, and StreamQueues, uri and names where available. Supports Regex."
+
+        return _result
+    
+    def getPlaylistHelpString(self) -> str:
+        """
+        Get the Playlist-section of help-print as a string.
+
+        Returns:
+            str: a string of Playlist info
+        """
+
+        _result = ""
+        _result += "\n" + addPlaylistFlags + " [name: str] [? playWatchedStreams: bool] [? allowDuplicates: bool] [? streamSourceIds: list]: Add a Playlist with name: name, playWatchedStreams: if playback should play watched QueueStreams, allowDuplicates: should Playlist allow duplicate QueueStreams (only if the uri is the same), streamSourceIds: a list of StreamSources."
+        _result += "\n" + addPlaylistFromYouTubeFlags + " [youTubePlaylistUrl: str] [? name: str] [? playWatchedStreams: bool] [? allowDuplicates: bool]: Add a Playlist and populate it with QueueStreams from given YouTube playlist youTubePlaylistUrl, with name: name, playWatchedStreams: if playback should play watched streams, allowDuplicates: should Playlist allow duplicate QueueStreams (only if the uri is the same)."
+        _result += "\n" + deletePlaylistFlags + " [playlistIds or indices: list]: deletes Playlists indicated."
+        _result += "\n" + restoreSourceFlags + " [playlistIds or index: str]: restore soft deleted Playlist from database."
+        _result += "\n" + listPlaylistFlags + " [? includeSoftDeleted: bool]: List Playlists with indices that can be used instead of IDs in other commands."
+        _result += "\n" + detailsPlaylistFlags + " [playlistIds or indices: list] [? enableFetch: bool] [? enableFetch: bool]: Prints details about given playlist, with option for including StreamSources and QueueStreams."
+        _result += "\n" + fetchPlaylistSourcesFlags + " [playlistIds or indices: list] [? takeAfter: datetime] [? takeBefore: datetime]: Fetch new streams from StreamSources in Playlists indicated, e.g. if a Playlist has a YouTube channel as a source, and the channel uploads a new video, this video will be added to the Playlist. Optional arguments takeAfter: only fetch QueueStreams after this date, takeBefore: only fetch QueueStreams before this date. Dates formatted like \"2022-01-30\" (YYYY-MM-DD)."
+        _result += "\n" + prunePlaylistFlags + " [playlistIds or indices: list]: Prune Playlists indicated, deleteing watched QueueStreams."
+        _result += "\n" + purgePlaylistFlags + " [? includeSoftDeleted: bool] [? permanentlyDelete: bool]: Purge database indicated, removing IDs with no corresponding relation and deleteing StreamSources and QueueStreams with no linked IDs in Playlists."
+        _result += "\n" + resetPlaylistFetchFlags + " [playlistIds or indices: list]: Resets fetch status of StreamSources in a Playlist and deletes QueueStreams from Playlist."
+        _result += "\n" + playFlags + " [playlistId or index: str] [? starindex: int] [? shuffle: bool] [? repeat: bool]: Start playing stream from a Playlist, order and automation (like skipping already watched QueueStreams) depending on the input and Playlist."
+        _result += self.getPlaylistSwitchesHelpString()
+        
+        return _result
+    
+    def getPlaylistSwitchesHelpString(self) -> str:
+        """
+        Get the Playlist-section switches of help-print as a string.
+
+        Returns:
+            str: a string of Playlist switches info
+        """
+
+        _result = ""
+        _result += "\n\t" + quitSwitches + ": End current playback and contintue the program without playing anymore QueueStreams in Playlist. Only available while Playlist is playing."
+        _result += "\n\t" + skipSwitches + ": Skip current QueueStream playing. This QueueStream will not be marked as watched. Only available while Playlist is playing."
+        _result += "\n\t" + addCurrentToPlaylistSwitches + " [playlistId or index: str]: Add the current QueueStream to another Playlist indicated by ID on index. Only available while Playlist is playing."
+        _result += "\n\t" + printPlaybackDetailsSwitches + ": Prints details of current playing Playlist."
+        
+        return _result
+        
+    def getQueueStreamHelpString(self) -> str:
+        """
+        Get the QueueStream-section help-print as a string.
+
+        Returns:
+            str: a string of QueueStream info
+        """
+
+        _result = ""
+        _result += "\n" + addStreamFlags + " [playlistId or index: str] [uri: string] [? name: str]: Add a stream to a Playlist from ID or index, from uri: URL, and name: name (set automatically if not given)."
+        _result += "\n" + deleteStreamFlags + " [playlistId or index: str] [streamIds or indices: list]: delete QueueStreams from Playlist.")
+        _result += "\n" + restoreStreamFlags + " [playlistId or index: str] [streamIds or indices: str]: restore soft deleted QueueStreams from database.")
+
+        return _result
+    
+    def getStreamSourceHelpString(self) -> str:
+        """
+        Get the StreamSource-section help-print as a string.
+
+        Returns:
+            str: a string of StreamSource info
+        """
+
+        _result = ""
+        _result += "\n" + addSourcesFlags + " [playlistId or index: str] [uri: string] [? enableFetch: bool] [? name: str]: Add a StreamSources from uri: URL, enableFetch: if the Playlist should fetch new stream from this StreamSource, and name: name (set automatically if not given)."
+        # _result += "\n" + addSourcesFlags + " [playlistId or index: str] [uri: string] [? enableFetch: bool] [? backgroundContent: bool] [? name: str]: Add a StreamSources from uri: URL, enableFetch: if the Playlist should fetch new QueueStream from this StreamSource, backgroundContent; if the QueueStream from this source are things you would play in the background, and name: name (set automatically if not given)."
+        _result += "\n" + deleteSourceFlags + " [playlistId or index: str] [sourceIds or indices: str]: deletes StreamSources from database and Playlist if used anywhere."
+        _result += "\n" + restoreSourceFlags + " [playlistId or index: str] [sourceIds or indices: str]: restore soft deleted StreamSources from database."
+        _result += "\n" + listSourcesFlags + " [? includeSoftDeleted: bool]: Lists StreamSources with indices that can be used instead of IDs in other commands."
+
+        return _result
+    
+    def getMetaHelpString(self) -> str:
+        """
+        Get the meta-section help-print as a string.
+
+        Returns:
+            str: a string of meta info
+        """
+
+        _result = ""
+        _result += "\n" + listSettingsFlags + ": Lists settings currently used by program. These settings can also be found in the file named \".env\" with examples in the file \".env-example\"."
+        _result += "\n" + listSoftDeletedFlags + " [? simplified: bool]: Lists all soft deleted entities. Option for simplified, less verbose list."
+
+        return _result

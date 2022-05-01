92730a228573c264b85660a7c03251141d39caef
Alexander <alex.thogar@gmail.com>
2022-05-02 00:07:02 +0200

    Removed _

--- FetchService.py
+++ FetchService.py
@@ -51,49 +51,49 @@ class FetchService():
         if(batchSize < 1):
             raise ArgumentException("fetch - batchSize was less than 1.")
 
-        _playlist = self.playlistService.get(playlistId)
-        if(_playlist == None):
+        playlist = self.playlistService.get(playlistId)
+        if(playlist == None):
             return 0
 
-        _newStreams = []
-        for _sourceId in _playlist.streamSourceIds:
-            _source = self.streamSourceService.get(_sourceId)
+        newStreams = []
+        for sourceId in playlist.streamSourceIds:
+            source = self.streamSourceService.get(sourceId)
             
-            if(_source == None):
-                printS("StreamSource with ID ", _sourceId, " could not be found. Consider removing it using the purge commands.", color = BashColor.FAIL)
+            if(source == None):
+                printS("StreamSource with ID ", sourceId, " could not be found. Consider removing it using the purge commands.", color = BashColor.FAIL)
                 continue
             
-            if(not _source.enableFetch):
+            if(not source.enableFetch):
                 continue
 
-            _fetchedStreams = []
-            _takeAfter = takeAfter if(not takeNewOnly) else _source.lastSuccessfulFetched
+            fetchedStreams = []
+            _takeAfter = takeAfter if(not takeNewOnly) else source.lastSuccessfulFetched
             
-            if(_source.isWeb):
-                if(_source.streamSourceTypeId == StreamSourceType.YOUTUBE.value):
-                    _fetchedStreams = self.fetchYoutube(_source, batchSize, _takeAfter, takeBefore, takeNewOnly)
+            if(source.isWeb):
+                if(source.streamSourceTypeId == StreamSourceType.YOUTUBE.value):
+                    fetchedStreams = self.fetchYoutube(source, batchSize, _takeAfter, takeBefore, takeNewOnly)
                 else:
                     # TODO handle other sources
                     continue
             else:
-                _fetchedStreams = self.fetchDirectory(_source, batchSize, _takeAfter, takeBefore, takeNewOnly)
+                fetchedStreams = self.fetchDirectory(source, batchSize, _takeAfter, takeBefore, takeNewOnly)
 
-            if(len(_fetchedStreams) > 0):
-                _source.lastSuccessfulFetched = datetime.now()
+            if(len(fetchedStreams) > 0):
+                source.lastSuccessfulFetched = datetime.now()
             
-            _source.lastFetchedIds = _fetchedStreams[1]
-            _source.lastFetched = datetime.now()
-            _updateSuccess = self.streamSourceService.update(_source)
-            if(_updateSuccess):
-                _newStreams += _fetchedStreams[0]
+            source.lastFetchedIds = fetchedStreams[1]
+            source.lastFetched = datetime.now()
+            updateSuccess = self.streamSourceService.update(source)
+            if(updateSuccess):
+                newStreams += fetchedStreams[0]
             else:
-                printS("Could not update StreamSource \"", _source.name, "\" (ID: ", _source.id, "), streams could not be added: \n", _fetchedStreams, color = BashColor.WARNING)
+                printS("Could not update StreamSource \"", source.name, "\" (ID: ", source.id, "), streams could not be added: \n", fetchedStreams, color = BashColor.WARNING)
                 
             sys.stdout.flush()
 
-        _updateResult = self.playlistService.addStreams(_playlist.id, _newStreams)
-        if(len(_updateResult) > 0):
-            return len(_newStreams)
+        updateResult = self.playlistService.addStreams(playlist.id, newStreams)
+        if(len(updateResult) > 0):
+            return len(newStreams)
         else:
             return 0
 
@@ -114,62 +114,62 @@ class FetchService():
         if(streamSource == None):
             raise ArgumentException("fetchYoutube - streamSource was None.")
 
-        _emptyReturn = ([], streamSource.lastFetchedIds)
-        _channel = Channel(streamSource.uri)
+        emptyReturn = ([], streamSource.lastFetchedIds)
+        channel = Channel(streamSource.uri)
 
-        if(_channel == None or _channel.channel_name == None):
+        if(channel == None or channel.channel_name == None):
             printS("Channel \"", streamSource.name, "\" (URL: ", streamSource.uri, ") could not be found or is not valid. Please remove it and add it back.", color = BashColor.FAIL)
-            return _emptyReturn
+            return emptyReturn
 
-        printS("Fetching videos from ", _channel.channel_name, "...")
+        printS("Fetching videos from ", channel.channel_name, "...")
         sys.stdout.flush()
-        if(len(_channel.video_urls) < 1):
-            printS("Channel \"", _channel.channel_name, "\" has no videos.", color = BashColor.WARNING)
-            return _emptyReturn
+        if(len(channel.video_urls) < 1):
+            printS("Channel \"", channel.channel_name, "\" has no videos.", color = BashColor.WARNING)
+            return emptyReturn
 
-        _newStreams = []
-        _streams = list(_channel.videos)
-        _lastStreamId = _streams[0].video_id
-        if(takeNewOnly and takeAfter == None and _lastStreamId in streamSource.lastFetchedIds):
-            printS("DEBUG: fetchYoutube - last video fetched: \"", sanitize(_streams[0].title), "\", YouTube ID \"", _lastStreamId, "\"", color = BashColor.WARNING)
-            printS("DEBUG: fetchYoutube - return due to takeNewOnly and takeAfter == None and _lastStreamId in streamSource.lastFetchedIds", color = BashColor.WARNING)
-            return _emptyReturn
+        newStreams = []
+        streams = list(channel.videos)
+        lastStreamId = streams[0].video_id
+        if(takeNewOnly and takeAfter == None and lastStreamId in streamSource.lastFetchedIds):
+            printS("DEBUG: fetchYoutube - last video fetched: \"", sanitize(streams[0].title), "\", YouTube ID \"", lastStreamId, "\"", color = BashColor.WARNING)
+            printS("DEBUG: fetchYoutube - return due to takeNewOnly and takeAfter == None and lastStreamId in streamSource.lastFetchedIds", color = BashColor.WARNING)
+            return emptyReturn
             
-        for i, _stream in enumerate(_streams):
-            if(takeNewOnly and _stream.video_id in streamSource.lastFetchedIds):
-                printS("DEBUG: fetchYoutube - name \"", sanitize(_stream.title), "\", YouTube ID \"", _stream.video_id, "\"", color = BashColor.WARNING)
-                printS("DEBUG: fetchYoutube - break due to takeNewOnly and _stream.video_id in streamSource.lastFetchedIds", color = BashColor.WARNING)
+        for i, stream in enumerate(streams):
+            if(takeNewOnly and stream.video_id in streamSource.lastFetchedIds):
+                printS("DEBUG: fetchYoutube - name \"", sanitize(stream.title), "\", YouTube ID \"", stream.video_id, "\"", color = BashColor.WARNING)
+                printS("DEBUG: fetchYoutube - break due to takeNewOnly and stream.video_id in streamSource.lastFetchedIds", color = BashColor.WARNING)
                 break
-            elif(not takeNewOnly and takeAfter != None and _stream.publish_date < takeAfter):
-                printS("DEBUG: fetchYoutube - break due to not takeNewOnly and takeAfter != None and _stream.publish_date < takeAfter", color = BashColor.WARNING)
+            elif(not takeNewOnly and takeAfter != None and stream.publish_date < takeAfter):
+                printS("DEBUG: fetchYoutube - break due to not takeNewOnly and takeAfter != None and stream.publish_date < takeAfter", color = BashColor.WARNING)
                 break
-            elif(not takeNewOnly and takeBefore != None and _stream.publish_date > takeBefore):
-                printS("DEBUG: fetchYoutube - continue due to not takeNewOnly and takeBefore != None and _stream.publish_date > takeBefore", color = BashColor.WARNING)
+            elif(not takeNewOnly and takeBefore != None and stream.publish_date > takeBefore):
+                printS("DEBUG: fetchYoutube - continue due to not takeNewOnly and takeBefore != None and stream.publish_date > takeBefore", color = BashColor.WARNING)
                 continue
             elif(i > batchSize):
                 printS("DEBUG: fetchYoutube - break due to i > batchSize", color = BashColor.WARNING)
                 break
             
-            _sanitizedTitle = sanitize(_stream.title)
-            printS("\tAdding \"", _sanitizedTitle, "\"...")
-            _stream = QueueStream(name = _sanitizedTitle, 
-                                           uri = _stream.watch_url, 
+            sanitizedTitle = sanitize(stream.title)
+            printS("\tAdding \"", sanitizedTitle, "\"...")
+            stream = QueueStream(name = sanitizedTitle, 
+                                           uri = stream.watch_url, 
                                            isWeb = True,
                                            streamSourceId = streamSource.id,
                                            watched = None,
                                            backgroundContent = streamSource.backgroundContent,
                                            added = datetime.now())
-            _newStreams.append(_stream)
+            newStreams.append(stream)
             
-        if(len(_newStreams) == 0):
+        if(len(newStreams) == 0):
             printS("No new videos detected.", color = BashColor.OKGREEN)
-            return _emptyReturn
+            return emptyReturn
             
-        streamSource.lastFetchedIds.append(_lastStreamId)
+        streamSource.lastFetchedIds.append(lastStreamId)
         if(len(streamSource.lastFetchedIds) > batchSize):
             streamSource.lastFetchedIds.pop(0)
         
-        return (_newStreams, streamSource.lastFetchedIds)
+        return (newStreams, streamSource.lastFetchedIds)
 
     def fetchDirectory(self, streamSource: StreamSource, batchSize: int = 10, takeAfter: datetime = None, takeBefore: datetime = None, takeNewOnly: bool = False) -> tuple[List[QueueStream], str]:
         """
@@ -188,8 +188,8 @@ class FetchService():
         if(streamSource == None):
             raise ValueError("fetchDirectory - streamSource was None")
 
-        _emptyReturn = ([], streamSource.lastFetchedIds)
-        return _emptyReturn
+        emptyReturn = ([], streamSource.lastFetchedIds)
+        return emptyReturn
     
     def resetPlaylistFetch(self, playlistIds: List[str]) -> int:
         """
@@ -202,26 +202,26 @@ class FetchService():
             int: number of playlists reset
         """
         
-        _result = 0
+        result = 0
         for playlistId in playlistIds:            
-            _playlist = self.playlistService.get(playlistId)
-            _deleteUpdateResult = True
+            playlist = self.playlistService.get(playlistId)
+            deleteUpdateResult = True
             
-            for queueStreamId in _playlist.streamIds:
-                _deleteStreamResult = self.queueStreamService.delete(queueStreamId)
-                _deleteUpdateResult = _deleteUpdateResult and _deleteStreamResult != None
+            for queueStreamId in playlist.streamIds:
+                deleteStreamResult = self.queueStreamService.delete(queueStreamId)
+                deleteUpdateResult = deleteUpdateResult and deleteStreamResult != None
             
-            _playlist.streamIds = []
-            _updateplaylistResult = self.playlistService.update(_playlist)
-            _deleteUpdateResult = _deleteUpdateResult and _updateplaylistResult != None
+            playlist.streamIds = []
+            updateplaylistResult = self.playlistService.update(playlist)
+            deleteUpdateResult = deleteUpdateResult and updateplaylistResult != None
             
-            for streamSourceId in _playlist.streamSourceIds:
-                _streamSource = self.streamSourceService.get(streamSourceId)
-                _streamSource.lastFetched = None
-                _updateStreamResult = self.streamSourceService.update(_streamSource)
-                _deleteUpdateResult = _deleteUpdateResult and _updateStreamResult != None
+            for streamSourceId in playlist.streamSourceIds:
+                streamSource = self.streamSourceService.get(streamSourceId)
+                streamSource.lastFetched = None
+                updateStreamResult = self.streamSourceService.update(streamSource)
+                deleteUpdateResult = deleteUpdateResult and updateStreamResult != None
             
-            if(_deleteUpdateResult):
-                _result += 1
+            if(deleteUpdateResult):
+                result += 1
                 
-        return _result
+        return result

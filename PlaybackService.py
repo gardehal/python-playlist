11acfd768325df1d4fabcf9175ccb94e0d24e33a
Alexander <alex.thogar@gmail.com>
2022-05-03 08:01:40 +0200

    Removed underscores from vars

--- PlaybackService.py
+++ PlaybackService.py
@@ -1,7 +1,6 @@
 import os
 import random
 import subprocess
-import sys
 from datetime import datetime
 from typing import List
 
@@ -66,41 +65,41 @@ class PlaybackService():
             bool: finished = True
         """
 
-        _playlist = self.playlistService.get(playlistId)
-        if(_playlist == None):
+        playlist = self.playlistService.get(playlistId)
+        if(playlist == None):
             return False
 
-        if(len(_playlist.streamIds) == 0):
-            printS("No streams found in \"", _playlist.name, "\". Ending playback.")
+        if(len(playlist.streamIds) == 0):
+            printS("No streams found in \"", playlist.name, "\". Ending playback.")
             return False
 
-        _streams = []
-        _rawStreams = self.playlistService.getStreamsByPlaylistId(_playlist.id)
+        streams = []
+        rawStreams = self.playlistService.getStreamsByPlaylistId(playlist.id)
 
-        if(len(_rawStreams) == 0):
-            printS("Playlist \"", _playlist.name, "\" has ", len(_streams), " streams, but they could not be found in database (they may have been removed). Ending playback.")
+        if(len(rawStreams) == 0):
+            printS("Playlist \"", playlist.name, "\" has ", len(streams), " streams, but they could not be found in database (they may have been removed). Ending playback.")
             return False
 
-        _streams = _rawStreams[startIndex:]
+        streams = rawStreams[startIndex:]
         if(shuffle):
-            random.shuffle(_streams)
+            random.shuffle(streams)
 
-        printS("Playing ", _playlist.name, ".")
+        printS("Playing ", playlist.name, ".")
         printS("Starting at stream number: ", (startIndex + 1), ", shuffle is ", ("on" if shuffle else "off"), ", repeat playlist is ", ("on" if repeatPlaylist else "off"), ", played videos set to watched is ", ("on" if PLAYED_ALWAYS_WATCHED else "off"), ".")
 
-        _playResult = False
+        playResult = False
         try:
             if(True): # Playlist mode
-                _playResult = self.playCmd(_playlist, _streams)
+                playResult = self.playCmd(playlist, streams)
         except:
             printStack(doPrint = DEBUG)
 
-        printS("Playlist \"", _playlist.name, "\" finished.")
+        printS("Playlist \"", playlist.name, "\" finished.")
 
         if(repeatPlaylist):
             self.play(playlistId, startIndex, shuffle, repeatPlaylist)
 
-        return _playResult
+        return playResult
     
     def playCmd(self, playlist: Playlist, streams: List[QueueStream]) -> bool:
         """
@@ -116,8 +115,8 @@ class PlaybackService():
         
         for i, stream in enumerate(streams):
             if(stream.watched != None and not playlist.playWatchedStreams):
-                _checkLogsMessage = " Check your logs (" + WATCHED_LOG_FILEPATH + ") for date/time watched." if LOG_WATCHED else " Logging is disabled and date/time watched is not available."
-                printS("Stream \"", stream.name, "\" (ID: ", stream.id, ") has been marked as watched.", _checkLogsMessage, color = BashColor.WARNING)
+                checkLogsMessage = " Check your logs (" + WATCHED_LOG_FILEPATH + ") for date/time watched." if LOG_WATCHED else " Logging is disabled and date/time watched is not available."
+                printS("Stream \"", stream.name, "\" (ID: ", stream.id, ") has been marked as watched.", checkLogsMessage, color = BashColor.WARNING)
                 continue
 
             subprocessStream = None
@@ -132,15 +131,15 @@ class PlaybackService():
                 continue
 
             printS(f"{i} - Now playing \"{stream.name}\"" + ("..." if(i < (len(streams) - 1)) else ". This is the last stream in this playback, press enter to finish."), color = BashColor.BOLD)
-            _inputHandleing = self.handlePlaybackInput(playlist, stream)
-            if(_inputHandleing == 0):
+            inputHandleing = self.handlePlaybackInput(playlist, stream)
+            if(inputHandleing == 0):
                 printS("An error occurred while parsing inputs.", color = BashColor.ERROR)
                 return False
-            elif(_inputHandleing == 1):
+            elif(inputHandleing == 1):
                 pass
-            elif(_inputHandleing == 2):
+            elif(inputHandleing == 2):
                 continue
-            elif(_inputHandleing == 3):
+            elif(inputHandleing == 3):
                 break
             
             # subprocessStream.terminate() # TODO Doesn't seem to work with browser, at least not new tabs
@@ -154,8 +153,8 @@ class PlaybackService():
             if(PLAYED_ALWAYS_WATCHED):
                 stream.watched = now
                 
-                _updateSuccess = self.queueStreamService.update(stream)
-                if(not _updateSuccess):
+                updateSuccess = self.queueStreamService.update(stream)
+                if(not updateSuccess):
                     printS("\"", stream.name, "\" could not be updated as watched.", color=BashColor.WARNING)
                     
         return True
@@ -176,55 +175,55 @@ class PlaybackService():
         """
         
         while 1: # Infinite loop until a return is hit
-            _inputMessage = "\tPress enter to play next, \"skip\" to skip video, or \"quit\" to quit playback: "
+            inputMessage = "\tPress enter to play next, \"skip\" to skip video, or \"quit\" to quit playback: "
             self.skipInputs.append("skip") # Ensure skip and quit always available
             self.quitInputs.append("quit")
-            _input = sanitize(input(_inputMessage), mode = 2)
+            inputArgs = sanitize(input(inputMessage), mode = 2)
                 
-            if(_input.strip() == ""):
+            if(inputArgs.strip() == ""):
                 return 1
             
-            elif(len(self.skipInputs) > 0 and _input in self.skipInputs):
+            elif(len(self.skipInputs) > 0 and inputArgs in self.skipInputs):
                 printS("Skipping, will not be marked as watched.", color = BashColor.OKGREEN)
                 return 2
             
-            elif(len(self.quitInputs) > 0 and _input in self.quitInputs):
-                printS("Ending playback due to user input.", color = BashColor.OKGREEN)
+            elif(len(self.quitInputs) > 0 and inputArgs in self.quitInputs):
+                printS("Ending playback due to user inputArgs.", color = BashColor.OKGREEN)
                 return 3
             
-            elif(len(self.repeatInputs) > 0 and _input in self.repeatInputs):
+            elif(len(self.repeatInputs) > 0 and inputArgs in self.repeatInputs):
                 printS("Repeating.", color = BashColor.OKGREEN)
                 self.playCmd(playlist, [stream]) # A little weird with prints and continueing but it works
             
-            elif(len(self.listPlaylistInputs) > 0 and _input in self.listPlaylistInputs):
-                _result = self.playlistService.getAll()
-                if(len(_result) > 0):
-                    _nPlaylists = len(_result)
-                    _title = "\t" + str(_nPlaylists) + " Playlist(s)."
+            elif(len(self.listPlaylistInputs) > 0 and inputArgs in self.listPlaylistInputs):
+                result = self.playlistService.getAll()
+                if(len(result) > 0):
+                    nPlaylists = len(result)
+                    title = "\t" + str(nPlaylists) + " Playlist(s)."
                     
-                    _data = []
-                    for (i, _entry) in enumerate(_result):
-                        _data.append("\t" + str(i) + " - " + _entry.summaryString())
+                    data = []
+                    for (i, entry) in enumerate(result):
+                        data.append("\t" + str(i) + " - " + entry.summaryString())
                         
-                    printLists([_data], [_title])
+                    printLists([data], [title])
                 else:
                     printS("No Playlists found.", color = BashColor.WARNING)
             
-            elif(len(self.addToInputs) > 0 and " " in _input and _input.split(" ")[0] in self.addToInputs):
-                _idsIndices = _input.split(" ")[1:]
-                _crossAddPlaylistResult = self.addPlaybackStreamToPlaylist(stream, _idsIndices)
-                printS("\"", stream.name, "\" added to new Playlist \"", _crossAddPlaylistResult[0].name, "\".", color = BashColor.OKGREEN, doPrint = (len(_crossAddPlaylistResult) > 0))
-                printS("\"", stream.name, "\" could not be added to new Playlist.", color = BashColor.FAIL, doPrint = (len(_crossAddPlaylistResult) == 0))
+            elif(len(self.addToInputs) > 0 and " " in inputArgs and inputArgs.split(" ")[0] in self.addToInputs):
+                idsIndices = inputArgs.split(" ")[1:]
+                crossAddPlaylistResult = self.addPlaybackStreamToPlaylist(stream, idsIndices)
+                printS("\"", stream.name, "\" added to new Playlist \"", crossAddPlaylistResult[0].name, "\".", color = BashColor.OKGREEN, doPrint = (len(crossAddPlaylistResult) > 0))
+                printS("\"", stream.name, "\" could not be added to new Playlist.", color = BashColor.FAIL, doPrint = (len(crossAddPlaylistResult) == 0))
                 
-            elif(len(self.printDetailsInputs) > 0 and _input in self.printDetailsInputs):
+            elif(len(self.printDetailsInputs) > 0 and inputArgs in self.printDetailsInputs):
                 self.playlistService.printPlaylistDetails([playlist.id])
                 
-            elif(len(self.printHelpInputs) > 0 and _input in self.printHelpInputs):
-                _help = self.commands.getPlaylistArgumentsHelpString()
-                printS(_help)
+            elif(len(self.printHelpInputs) > 0 and inputArgs in self.printHelpInputs):
+                help = self.commands.getPlaylistArgumentsHelpString()
+                printS(help)
                 
             else:
-                printS("Argument(s) not recognized: \"", _input, "\". Please refrain from using arrows to navigate in the CLI as it adds hidden characters.", color = BashColor.WARNING)
+                printS("Argument(s) not recognized: \"", inputArgs, "\". Please refrain from using arrows to navigate in the CLI as it adds hidden characters.", color = BashColor.WARNING)
             
         return 0
 
@@ -240,20 +239,20 @@ class PlaybackService():
             Playlist: Playlist the stream was added to
         """
         
-        _result = []
+        result = []
         if(len(idsIndices) < 1):
             printS("Missing arguments, cross-adding stream requires IDs of Playlists to add to.", color = BashColor.WARNING)
-            return _result
+            return result
         
-        _ids = getIdsFromInput(idsIndices, self.playlistService.getAllIds(), self.playlistService.getAll(), debug = DEBUG)
-        if(len(_ids) == 0):
+        ids = getIdsFromInput(idsIndices, self.playlistService.getAllIds(), self.playlistService.getAll(), debug = DEBUG)
+        if(len(ids) == 0):
             printS("Failed to add cross-add streams, missing playlistIds or indices.", color = BashColor.WARNING)
-            return _result
+            return result
         
-        for _id in _ids:
-            _addResult = self.playlistService.addStreams(_id, [queueStream])
-            if(len(_addResult) > 0):
-                _playlist = self.playlistService.get(_id)
-                _result.append(_playlist)
+        for id in ids:
+            addResult = self.playlistService.addStreams(id, [queueStream])
+            if(len(addResult) > 0):
+                playlist = self.playlistService.get(id)
+                result.append(playlist)
         
-        return _result
+        return result

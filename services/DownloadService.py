072a885e1c95350f39dfabe09c65327eb55feb33
Alexander <alethogar@protonmail.com>
2023-05-28 01:18:20 +0200

    Fixed some print issues and title for html fetch

--- services/DownloadService.py
+++ services/DownloadService.py
@@ -1,8 +1,9 @@
 import json
 import os
+import sys
+import urllib.request
 
 import mechanize
-import urllib.request
 from bs4 import BeautifulSoup
 from grdUtil.BashColor import BashColor
 from grdUtil.DateTimeUtil import getDateTimeAsNumber
@@ -17,6 +18,7 @@ from Settings import Settings
 
 class DownloadService():
     settings: Settings = None
+    
     def __init__(self):
         self.settings = Settings()
         
@@ -53,7 +55,7 @@ class DownloadService():
         """
         
         youtube = YouTube(url)
-        printD("Downloading video from ", url)
+        printS("Downloading video from ", url)
         videoPath = self.getVideoPath("youtube", youtube.title, fileExtension)
         youtube.streams.filter(progressive = True, file_extension = fileExtension).order_by("resolution").desc().first().download(videoPath)
                 
@@ -74,25 +76,38 @@ class DownloadService():
         br = mechanize.Browser()
         br.set_handle_robots(False)
         br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        videoTitle = None
         fileUrl = None
         try:
+            printS("Fetching data for video ", url, " ...")
+            sys.stdout.flush()
             br.open(url)
             html = br.response().read()
             document = BeautifulSoup(html, 'html.parser')
             jsonString = document.find("script", { "type": "application/ld+json" }).text.strip()
+            printD(jsonString, debug = (self.settings.debug and False))
+            printD("Reading JSON...", debug = self.settings.debug)
+            sys.stdout.flush()
             jsonData = json.loads(jsonString)
-            contentUrlExpression = parse("$.contentUrl")
-            fileUrl = contentUrlExpression.find(jsonData)[0].value
+            videoTitle = parse("$.name").find(jsonData)[0].value
+            fileUrl = parse("$.contentUrl").find(jsonData)[0].value
+            printD("File URL: ", fileUrl, debug = self.settings.debug)
+            sys.stdout.flush()
         except:
             printS("Failed getting video for ", url, color = BashColor.FAIL)
-            return "" # throw x
+            return "" # TODO throw x
+        
+        if(videoTitle == None):
+            videoTitle = "unknown_video"
+            printS("Failed getting title, defaulting to ", videoTitle, color = BashColor.FAIL)
         
         if(fileUrl == None):
             printS("Failed getting URL to video for ", url, color = BashColor.FAIL)
-            return "" # throw x
+            return "" # TODO throw x
         
-        printD("Downloading video from ", url)
-        videoPath = self.getVideoPath("odysee", "test", fileExtension)
+        printS("Downloading video from ", url)
+        sys.stdout.flush()
+        videoPath = self.getVideoPath("odysee", videoTitle, fileExtension)
         urllib.request.urlretrieve(fileUrl, videoPath) 
                 
         return videoPath

4336c19a4101a9e91926035e9962c7183aca38b8
Alexander <alethogar@protonmail.com>
2023-05-28 16:46:00 +0200

    Fixed scraper rejected as a bot (it's not, it's just a script called by a user)

--- services/SharedService.py
+++ services/SharedService.py
@@ -55,6 +55,8 @@ class SharedService():
                 printD("Getting title from mechanize.", color = BashColor.WARNING, debug = self.settings.debug)
                 try:
                     br = mechanize.Browser()
+                    br.set_handle_robots(False)
+                    br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
                     br.open(url)
                     title = br.title()
                     br.close()

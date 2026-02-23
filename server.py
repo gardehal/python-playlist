1a03ab183f5480db9edee935d5d83e77c58a4597
Alexander <alethogar@protonmail.com>
2026-02-24 00:12:31 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,11 +1,14 @@
 from flask import Flask, render_template
 from services.PlaylistService import PlaylistService
+from services.QueueStreamService import QueueStreamService
+from services.StreamSourceService import StreamSourceService
 
 app = Flask(__name__)
 
 playlistService: PlaylistService = PlaylistService()
-# https://www.youtube.com/watch?v=jQjjqEjZK58
-    
+queueStreamService: QueueStreamService = QueueStreamService()
+streamSourceService: StreamSourceService = StreamSourceService()
+
 @app.route("/")
 @app.route("/index")
 def index():
@@ -16,28 +19,47 @@ def index():
 def help():
     return render_template("help.html")
 
+@app.route("/quit")
+def quitApp():
+    quit() # ?
+    return
+
 @app.route("/playlists")
-def playlistIndex():
+def playlistsIndex():
     playlists = playlistService.getAll()
     return render_template("playlists/index.html", playlists= playlists)
 
-@app.route("/playlists/1")
-def playlistDetails():
-    return render_template("playlists/details.html")
+@app.route("/playlists/<id>")
+def playlistsDetails(id: str):
+    playlist = playlistService.get(id)
+    queueStreams = playlistService.getStreamsByPlaylistId(id)
+    streamSources = playlistService.getSourcesByPlaylistId(id)
+    return render_template("playlists/details.html", playlist= playlist, queueStreams= queueStreams, streamSources= streamSources)
 
-@app.route("/sources")
-@app.route("/streamsources")
-def sourcesIndex():
-    return render_template("streamsources/index.html")
+@app.route("/queueStreams")
+def queueStreamsIndex():
+    queueStreams = queueStreamService.getAll()
+    return render_template("queueStreams/index.html", queueStreams= queueStreams)
 
-@app.route("/streamsources/1")
-def sourcesDetails():
-    return render_template("streamsources/details.html")
+@app.route("/queueStreams/<id>")
+def queueStreamsDetails(id: str):
+    queueStream = queueStreamService.get(id)
+    return render_template("queueStreams/details.html", queueStream= queueStream)
 
-@app.route("/steams")
-@app.route("/queuestreams")
-def streamIndex():
-    return render_template("streamsources/index.html")
+@app.route("/streamSources")
+def streamSourcesIndex():
+    streamSources = streamSourceService.getAll()
+    return render_template("streamSources/index.html", streamSources= streamSources)
+
+@app.route("/streamSources/<id>")
+def streamSourcesDetails(id: str):
+    streamSource = streamSourceService.get(id)
+    return render_template("streamSources/details.html", streamSource= streamSource)
+
+@app.route("/play/<playlistId>/<index>")
+def play(playlistId: str, index: int):
+    # TODO
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888)

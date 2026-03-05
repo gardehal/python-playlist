de1e5a4edf06c9c4d875b5364c418bf56aeaea53
Alexander <alethogar@protonmail.com>
2026-03-05 05:00:19 +0100

    Fixing some form values

--- forms/PlaylistForm.py
+++ forms/PlaylistForm.py
@@ -1,13 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, NumberRange
 
 class PlaylistForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired(), Length(0, 40)])
-    description = StringField("Description", validators=[Length(0, 100)])
-    sortOrder = IntegerField("Sort order")
+    name = StringField("Name", validators=[DataRequired(), Length(0, 200)])
+    description = StringField("Description", validators=[Length(0, 200)])
+    sortOrder = IntegerField("Sort order", validators=[NumberRange(0)], default=99)
     playWatchedStreams = BooleanField("Play watched streams", default=False)
     allowDuplicates = BooleanField("Allow duplicates", default=False)
     favorite = BooleanField("Favorite", default=False)
     
     submit = SubmitField("Save")
+    # cancel = SubmitField("Cancel")
--- forms/StreamSourceForm.py
+++ forms/StreamSourceForm.py
@@ -5,8 +5,8 @@ from wtforms.validators import DataRequired, Length, NumberRange
 class StreamSourceForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired(), Length(0, 200)])
     uri = StringField("URI", validators=[Length(0, 200)])
-    streamSourceTypeId = IntegerField("Type", validators=[NumberRange(1)])
     isWeb = BooleanField("Is web", default=False)
+    streamSourceTypeId = IntegerField("Type", validators=[NumberRange(1)])
     enableFetch = BooleanField("Enable fetch", default=False)
     backgroundContent = BooleanField("Background content", default=False)
     alwaysDownload = BooleanField("Always download", default=False)
